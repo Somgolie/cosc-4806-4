5d018840eeb2a57ad4503ed08ea4e83474aff115
Somgolie <sejinkeonye@algomau.ca>
2025-07-05 00:50:37 +0000

    added edit view in reminder

---
+++ app/views/reminders/edit.php
@@ -0,0 +1,8 @@
+<?php require_once 'app/views/templates/header.php'; ?>
+<div class="container">
+    <div class="page-header mb-4">
+        <h1>Edit Reminder</h1>
+    </div>
+
+
+<?php require_once 'app/views/templates/footer.php'; ?>
