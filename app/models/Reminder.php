5db5acf8cdbdbbcb73c3bdf08a1480fa2f987d8f
Somgolie <sejinkeonye@algomau.ca>
2025-07-05 00:41:40 +0000

    made the create view functional

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -18,5 +18,12 @@ class Reminder {
       $db = db_connect();
 
     }
+    public function create_reminder($user_id, $subject) {
+        $db = db_connect();
+        $stmt = $db->prepare("INSERT INTO reminders (user_id, subject, time_created) VALUES (:user_id, :subject, NOW())");
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->bindParam(':subject', $subject, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
 }
 ?>
