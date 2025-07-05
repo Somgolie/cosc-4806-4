1853b938c9ffa3f9bf75c5d75663b5e02f8fb8b0
Somgolie <sejinkeonye@algomau.ca>
2025-07-05 02:15:49 +0000

    edited header dropdown text

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -31,7 +31,7 @@ if (!isset($_SESSION['auth'])) {
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Actions
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Action 1</a></li>
