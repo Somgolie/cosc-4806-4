b932b6c9f9c57f66b1e7d02a7583318920a89f79
Somgolie <sejinkeonye@algomau.ca>
2025-07-05 01:39:02 +0000

    renamed the table from reminder to notes.

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -8,7 +8,7 @@ class Reminder {
 
     public function get_reminders () {
       $db = db_connect();
-      $statement = $db->prepare("SELECT * FROM reminders;");
+      $statement = $db->prepare("SELECT * FROM notes;");
       $statement->execute();
       $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
       return $rows;
@@ -17,14 +17,14 @@ class Reminder {
 
     public function create_reminder($user_id, $subject) {
         $db = db_connect();
-        $stmt = $db->prepare("INSERT INTO reminders (user_id, subject, time_created) VALUES (:user_id, :subject, NOW())");
+        $stmt = $db->prepare("INSERT INTO notes (user_id, subject, time_created) VALUES (:user_id, :subject, NOW())");
         $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
         $stmt->bindParam(':subject', $subject, PDO::PARAM_STR);
         return $stmt->execute();
     }
   public function get_reminder_by_id($id) {
       $db = db_connect();
-      $stmt = $db->prepare("SELECT * FROM reminders WHERE id = :id");
+      $stmt = $db->prepare("SELECT * FROM notes WHERE id = :id");
       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
       $stmt->execute();
       return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -32,14 +32,14 @@ class Reminder {
 
   public function update_reminder($id, $subject) {
       $db = db_connect();
-      $stmt = $db->prepare("UPDATE reminders SET subject = :subject WHERE id = :id");
+      $stmt = $db->prepare("UPDATE notes SET subject = :subject WHERE id = :id");
       $stmt->bindParam(':subject', $subject, PDO::PARAM_STR);
       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
       return $stmt->execute();
   }
   public function delete_reminder($id) {
       $db = db_connect();
-      $stmt = $db->prepare("DELETE FROM reminders WHERE id = :id");
+      $stmt = $db->prepare("DELETE FROM notes WHERE id = :id");
       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
       return $stmt->execute();
   }
