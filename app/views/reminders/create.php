055ff7b0ecca5facbc7064a0f219cd367f6a7609
Somgolie <sejinkeonye@algomau.ca>
2025-07-05 00:39:14 +0000

    made new reminders/create view page

---
+++ app/views/reminders/create.php
@@ -0,0 +1,8 @@
+<?php require_once 'app/views/templates/header.php'; ?>
+<div class="container">
+    <div class="page-header mb-4">
+        <h1>Create Reminder</h1>
+    </div>
+
+</div>
+<?php require_once 'app/views/templates/footer.php'; ?>
